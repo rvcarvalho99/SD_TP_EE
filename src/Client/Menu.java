6e7fc61dd48450bf9bdf11f19cf9e31c2c58cf85
ZePinto99 <z.miguelpinto@gmail.com>
2020-08-29 15:32:06 +0100

    UploadClient

--- src/Client/Menu.java
+++ src/Client/Menu.java
@@ -22,24 +22,51 @@ public class Menu {
         switch (read){
             case "1":
                 System.out.println("Download");
+                out.writeInt(1);
                 break;
             case "2":
-                System.out.println("Uplaod");
+                System.out.println("Upload");
+                out.writeInt(2);
+
+                System.out.println("Titulo:");
+                String titulo = input.readLine();
+                out.writeUTF(titulo);
+
+                System.out.println("Artista:");
+                String artista = input.readLine();
+                out.writeUTF(artista);
+
+                System.out.println("Ano:");
+                //try catch caso se faça merda no ano
+                int ano = Integer.parseInt(input.readLine());
+                out.writeInt(ano);
+
+                System.out.println("Path:");
+                String path = input.readLine();
+
+                //enviar o ficheiro
+
                 break;
             case "3":
                 System.out.println("Criar PlayList");
+                out.writeInt(3);
                 break;
             case "4":
                 System.out.println("Adicionar a PlayList");
+                out.writeInt(4);
                 break;
             case "5":
                 System.out.println("Ver PlayLists");
+                out.writeInt(5);
                 break;
             case "6":
+                out.writeInt(6);
                 System.out.println("Ver info de Musica");
                 break;
             case "7":
+                out.writeInt(7);
                 System.out.println("Alterar Password");
+
                 break;
         }
     }
